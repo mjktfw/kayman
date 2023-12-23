70c72dcb4ab1e4534cc3576d05d3cb75d6b9e4b5
mjktfw <marcinjakubkaminski@gmail.com>
2023-12-23 23:22:04 +0100

    .

---
+++ linux/pubkey-add-repo.bash
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+source .env
+
+repo_local_path="${HOME}/pit/${GH_REPO_NAME}"
+key_pub_path="${HOME}/.ssh/${KEY_NAME}.pub"
+
+cp "${key_pub_path}" "${repo_local_path}"
+
+git add -a "${repo_local_path}" &&
+    git commit -m "${KEY_NAME}" &&
+    git push

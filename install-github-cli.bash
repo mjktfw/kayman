58d2958d6660b1ba12a111613cb34ea723ba5739
mjktfw <marcinjakubkaminski@gmail.com>
2023-12-23 23:00:33 +0100

    .

--- install-github-cli.bash
+++ install-github-cli.bash
@@ -4,11 +4,9 @@
 
 type -p curl >/dev/null || (sudo apt -y update && sudo apt -y install curl -y)
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | 
-    sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg  &&
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | 
-    sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 sudo apt -y update &&
     sudo apt -y install gh

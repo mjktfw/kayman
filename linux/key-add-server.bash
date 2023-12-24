7e362e54062953c2aea57ba217b6a21bba513a46
mjktfw <marcinjakubkaminski@gmail.com>
2023-12-24 11:58:55 +0100

    .

--- linux/key-add-server.bash
+++ linux/key-add-server.bash
@@ -2,6 +2,25 @@
 
 source .env
 
+OPTSTRING=":u:r:k"
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    u)
+      GH_USERNAME="${OPTARG}"
+      ;;
+    r)
+      GH_REPO_NAME="${OPTARG}"
+      ;;
+    k)
+      KEY_NAME="${OPTARG}"
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
 gh_baseurl="https://raw.githubusercontent.com/${GH_USERNAME}/${GH_REPO_NAME}/master"
 gh_key_url="${gh_baseurl}/${KEY_NAME}.pub"
 

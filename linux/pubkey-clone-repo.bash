455bc6f475ad0d909e44d8f3541e2e2e8c879afd
mjktfw <marcinjakubkaminski@gmail.com>
2023-12-24 12:14:37 +0100

    .

--- linux/pubkey-clone-repo.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-source .env
-
-repo_local_path="${HOME}/pit/${GH_REPO_NAME}"
-repo_remote_path="ssh://git@github.com/${GH_USERNAME}/${GH_REPO_NAME}.git"
-
-git clone "${repo_remote_path}" "${repo_local_path}"
